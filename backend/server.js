e90cef87e6a9f4602a366a04cbba20de0c4b1cda
hashPirate <69019512+hashPirate@users.noreply.github.com>
2025-11-03 10:49:43 -0600

    Added basic routing functionality.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 const dotenv = require('dotenv').config();
+const path = require('path')
 
 // Create express app
 const app = express();
