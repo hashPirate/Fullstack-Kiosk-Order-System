a2051c3615ce911a5c1278000fdd6d5dbc46147c
hashPirate <69019512+hashPirate@users.noreply.github.com>
2025-11-03 09:50:52 -0600

    Moved /index.js to /backend/server.js (for real this time)

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,52 @@
-import express from "express";
-import dotenv from "dotenv";
-import {connectDB} from "./config/db.js";
+const express = require('express');
+const { Pool } = require('pg');
+const dotenv = require('dotenv').config();
 
-dotenv.config();
-const PORT = process.env.PORT;
-
+// Create express app
 const app = express();
+const port = 3000;
 
-app.use(express.json());    // enable JSON support so that it can read JSON requests
-
-app.use("/api/menuParts", menuParts);
-
-app.listen(5000, () => {
-    connectDB();
-    console.log("Server started on port:", PORT);
+// Create pool
+const pool = new Pool({
+    user: process.env.PSQL_USER,
+    host: process.env.PSQL_HOST,
+    database: process.env.PSQL_DATABASE,
+    password: process.env.PSQL_PASSWORD,
+    port: process.env.PSQL_PORT,
+    ssl: {rejectUnauthorized: false}
 });
 
+// Add process hook to shutdown pool
+process.on('SIGINT', function() {
+    pool.end();
+    console.log('Application successfully shutdown');
+    process.exit(0);
+});
+
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+    const data = {name: 'Mario'};
+    res.render('index', data);
+});
+
+app.get('/user', (req, res) => {
+    teammembers = []
+    pool
+        .query('SELECT * FROM teammembers;')
+        .then(query_res => {
+            for (let i = 0; i < query_res.rowCount; i++){
+                teammembers.push(query_res.rows[i]);
+            }
+            const data = {teammembers: teammembers};
+            console.log(teammembers);
+            res.render('user', data);        
+        });
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
 
 
