7f02d738a7ede564511bd3d4d6347c30a8b2caf9
hashPirate <69019512+hashPirate@users.noreply.github.com>
2025-11-09 20:09:53 -0600

    Accidentally commented out a line -- oops. Uncommented it.

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require('dotenv').config()     // [Donnell]: I wrote this because dotenv wasn't 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// .use(express.json());     // [Donnell]: I also had to add this for it to work. Idk why.
+app.use(express.json());     // [Donnell]: I also had to add this for it to work. Idk why.
 
 // Controllers
 const ingredientsController = require('./controllers/ingredients');
