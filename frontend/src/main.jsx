755c448892641d13d644c8fa61eafb6772145836
hashPirate <69019512+hashPirate@users.noreply.github.com>
2025-12-05 11:09:04 -0600

    Attempting to refactor to tanstack

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -29,60 +29,66 @@ import SalesReport from "./views/manager/views/SalesReport.jsx";
 import LoginRoute from './utilities/LoginRoute.jsx';
 import MenuBoardHome from "./views/menu/MenuBoard.jsx";
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import App from './App.jsx';
 
+// Provider for Tanstack query client.
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<App />} />
-        <Route path="/login" element={<LoginView />} />
-        <Route path="/logout" element={<LogoutView />} />
-        <Route path="/menu" element={
-          <MenuBoardHome />} />
-        <Route path="/kiosk" element={
-          <LoginRoute requiredAccess="kiosk">
-            <KioskView />
-          </LoginRoute>
-        }>
-          <Route index element={<KioskHome />}></Route>
-          <Route path="build_item" element={<BuildItem />}></Route>
-          <Route path="language" element={<SetLanguage />}></Route>
-          <Route path="cart" element={<KioskCart />}></Route>
-        </Route>
-        <Route path="/cashier" element={
-          <LoginRoute requiredAccess="cashier">
-            <CashierView />
-          </LoginRoute>
-        }>
-          <Route index element={<Navigate to="menu_items" replace />} />
-          <Route path="menu_items" element={<CashierMenuItems />} />
-          <Route path="menu_parts" element={<CashierMenuParts />} />
-        </Route>
-        <Route path="/kitchen" element={
-          <LoginRoute requiredAccess="kitchen">
-            <KitchenView />
-          </LoginRoute>
-        }>
-          <Route index element={<Navigate to="pending" replace />}></Route>
-          <Route path="pending" element={<KitchenPending />}></Route>
-          <Route path="completed" element={<KitchenCompleted />}></Route>
-        </Route>
-        <Route path="/manager" element={
-          <LoginRoute requiredAccess="manager">
-            <ManagerView />
-          </LoginRoute>
-        }>
-          <Route index element={<Navigate to="/manager/servers" replace />} />
-          <Route path="servers" element={<ManageServers />}></Route>
-          <Route path="inventory" element={<Inventory />}></Route>
-          <Route path="reports" element={<Reports />}></Route>
-          <Route path="menu-parts" element={<ManageMenuParts />}></Route>
-          <Route path="menu-items" element={<ManageMenuItems />}></Route>
-          <Route path="sales-report" element={<SalesReport />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<App />} />
+          <Route path="/login" element={<LoginView />} />
+          <Route path="/logout" element={<LogoutView />} />
+          <Route path="/menu" element={
+            <MenuBoardHome />} />
+          <Route path="/kiosk" element={
+            <LoginRoute requiredAccess="kiosk">
+              <KioskView />
+            </LoginRoute>
+          }>
+            <Route index element={<KioskHome />}></Route>
+            <Route path="build_item" element={<BuildItem />}></Route>
+            <Route path="language" element={<SetLanguage />}></Route>
+            <Route path="cart" element={<KioskCart />}></Route>
+          </Route>
+          <Route path="/cashier" element={
+            <LoginRoute requiredAccess="cashier">
+              <CashierView />
+            </LoginRoute>
+          }>
+            <Route index element={<Navigate to="menu_items" replace />} />
+            <Route path="menu_items" element={<CashierMenuItems />} />
+            <Route path="menu_parts" element={<CashierMenuParts />} />
+          </Route>
+          <Route path="/kitchen" element={
+            <LoginRoute requiredAccess="kitchen">
+              <KitchenView />
+            </LoginRoute>
+          }>
+            <Route index element={<Navigate to="pending" replace />}></Route>
+            <Route path="pending" element={<KitchenPending />}></Route>
+            <Route path="completed" element={<KitchenCompleted />}></Route>
+          </Route>
+          <Route path="/manager" element={
+            <LoginRoute requiredAccess="manager">
+              <ManagerView />
+            </LoginRoute>
+          }>
+            <Route index element={<Navigate to="/manager/servers" replace />} />
+            <Route path="servers" element={<ManageServers />}></Route>
+            <Route path="inventory" element={<Inventory />}></Route>
+            <Route path="reports" element={<Reports />}></Route>
+            <Route path="menu-parts" element={<ManageMenuParts />}></Route>
+            <Route path="menu-items" element={<ManageMenuItems />}></Route>
+            <Route path="sales-report" element={<SalesReport />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 )
--- frontend/src/views/kitchen/KitchenCompleted/KitchenCompleted.jsx
+++ frontend/src/views/kitchen/KitchenCompleted/KitchenCompleted.jsx
@@ -1,66 +1,49 @@
 
 import { NavLink, Outlet } from "react-router";
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from "axios";
 import LoadingPopup from "../LoadingPopup.jsx";
+import { HashLoader } from "react-spinners";
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 
 import styles from "./KitchenCompleted.module.css";
 import CompletedOrder from "./CompletedOrder.jsx";
 import PageSwitcher from "./PageSwitcher.jsx";
 
-export default function KitchenCompleted() {
-    // These are state because they do not need to be updated instantly and they
-    // should cause a re-render when updated.
-    const [orders, setOrders] = useState([]);
-    const [numOrders, setNumOrders] = useState(0);
-    const [loading, setLoading] = useState(true);
-    const [pageNum, setPageNum] = useState(1);    // Start at page 1
+// Number of orders that should appear per page.
+const pageOrderLimit = 10;
 
-    const pageOrderLimit = 10;
-    const totalPages = (numOrders / pageOrderLimit) + 1;
-
-    function getMenuItemsMapping() {
+async function getMenuItemsMapping() {
+    try {
         let menuItemsMapping = {};
-        axios.get('/api/menu/items').then((response) => {
-            for (const item of response.data) {
-                const {menu_item_id, ...itemRest} = item;
-                menuItemsMapping[menu_item_id] = itemRest;
-            }
-        }).catch((error) => {
-            console.log("Error while getting menu items mapping.");
-        });
-        return menuItemsMapping;
-    }
+        let response = await axios.get('/api/menu/items');
+        for (const item of response.data) {
+            const {menu_item_id, ...itemRest} = item;
+            menuItemsMapping[menu_item_id] = itemRest;
+        }
 
-    function fetchCookedOrders() {
+        return menuItemsMapping;
+    } catch (error) {
+        console.log(error);
+        return undefined;
+    }
+}
+
+async function fetchCookedOrders(pageNum) {
+    try {
         // Get the menu items mapping.
-        const menuItemsMapping = getMenuItemsMapping();
+        const menuItemsMapping = await getMenuItemsMapping();
 
         const limit = pageOrderLimit;
         const offset = limit * (pageNum - 1);
 
         // Get an array of all currently uncooked order IDs
-        let cookedOrders;
-        axios.get(`/api/orders/cooked?limit=${limit}&offset=${offset}`).then((response) => {
-            cookedOrders = response.data;
-        }).catch((error) => {
-            console.log("Error while fetching recent cooked orders.");
-        });
+        let cookedOrders = (await axios.get(`/api/orders/cooked?limit=${limit}&offset=${offset}`)).data;
 
         for (const order of cookedOrders) {
-            let orderItems;
-            axios.get(`/api/orders/${order.order_id}/items`).then((response => {
-                orderItems = response.data;
-            })).catch((error) => {
-                console.log("Error while getting order items.");
-            });
+            const orderItems = (await axios.get(`/api/orders/${order.order_id}/items`)).data;
             for (const item of orderItems) {
-                let menuParts;
-                axios.get(`/api/orders/items/${item.order_item_id}/parts`).then((response) => {
-                    menuParts = response.data;
-                }).catch((error) => {
-                    console.log("Error while getting order item menu parts.");
-                });
+                const menuParts = (await axios.get(`/api/orders/items/${item.order_item_id}/parts`)).data;
                 item.menu_parts = menuParts;
                 item.item_name = menuItemsMapping[item.menu_item_id].item_name;
             }
@@ -68,75 +51,66 @@ export default function KitchenCompleted() {
         }
 
         return cookedOrders;
+    } catch (error) {
+        console.log(error);
     }
+}
 
-    function refreshCookedOrders() {
-        console.log("Refreshing kitchen orders...");
+async function refreshCookedOrders(pageNum) {
+    console.log("Refreshing kitchen orders...");
 
-        // Get the number of cooked orders
-        let newNumOrders;
-        axios.get("/api/orders/cooked/count").then((response) => {
-            newNumOrders = response.data;
-        }).catch((error) => {
-            console.log("Error while getting count of cooked orders.");
-        });
-        setNumOrders(newNumOrders);
+    // Get the number of cooked orders
+    let numOrders = (await axios.get("/api/orders/cooked/count")).data;
+    const orders = await fetchCookedOrders(pageNum);
 
-        const newOrders = fetchCookedOrders();
-        setOrders(newOrders);
-    };
+    return {orders, numOrders}
+};
 
-    // On initial load.
-    useEffect(() => {
-        // Initial refresh
-        refreshCookedOrders();
-        setLoading(false);
+export default function KitchenCompleted() {
+    // These are state because they do not need to be updated instantly and they
+    // should cause a re-render when updated.
+    const [pageNum, setPageNum] = useState(1);    // Start at page 1
 
-        const msRefreshRate = 5000;
-        const ordersInterval = setInterval(refreshCookedOrders, msRefreshRate);
+    const queryClient = useQueryClient();
 
-        return () => {
-            clearInterval(ordersInterval);
-        };
-    }, []);
+    const { data, isLoading, error } = useQuery({
+        queryKey: ["completedOrders", pageNum],
+        queryFn: async () => { await refreshCookedOrders(pageNum) },
+        refetchInterval: 5000
+    });
 
-    // On pageNum change
-    useEffect(() => {
-        setLoading(true)
-        refreshCookedOrders();
-        setLoading(false)
-    }, [pageNum]);
+    const mutation = useMutation({
+        mutationKey: ['setIsCooked'],
+        mutationFn: async (payload) => {
+            const setCookedResponse = await axios.put(`/api/orders/${id}/set-cooked`, payload);
+            if (!setCookedResponse.data.success) {
+                throw new Error("Could not update is_cooked in db.");
+            }
+        },
+        onMutate: async () => {
+            // Cancel any pending refreshes.
+            await queryClient.cancelQueries({ queryKey: ['completedOrders'] });
+        },
+        onSettled: () => {
+            // Invalidate bc data has been updated.
+            queryClient.invalidateQueries({ queryKey: ['completedOrders'] });
+        }
+    });
 
     async function reviveOrder(id) {
         console.log(`Attempting to revive order ${id}...`);
         try {
-            // show loader while reviving
-            setLoading(true);
-
-            // non-optimistically revive order
-            const setCookedResponse = await axios.put(`/api/orders/${id}/set-cooked`, { is_cooked: false });
-            if (!setCookedResponse.data.success) {
-                console.log("ERROR: order could not actually be marked in database. Failing silently...");
-                return;
-            } else {
-                console.log("Order successfully marked in database.");
-            }
-            
-            // Then refresh page, so that there are always 10 elements.
-            await refreshCookedOrders();
-            console.log(`Successfully revived order ${id}!`);
+            mutation.mutate({ is_cooked: false });
         } catch (error) {
-            console.log("Error while trying to revive order:", error);
-        } finally {
-            setLoading(false);
+            console.log(error);
         }
     }
 
     function renderCookedOrders() {
-        if (!loading) {
+        if (!isLoading) {
             // Render main content if initial load is complete.
-            if (!orders.length == 0) {
-                return orders.map((ord, i) => <CompletedOrder key={ord.order_id} orderId={ord.order_id} menuItems={ord.menu_items} handleRemoveOrder={reviveOrder} />);
+            if (!data.orders.length == 0) {
+                return data.orders.map((ord, i) => <CompletedOrder key={ord.order_id} orderId={ord.order_id} menuItems={ord.menu_items} handleRemoveOrder={reviveOrder} />);
             } else {
                 return (
                     <>
@@ -147,16 +121,16 @@ export default function KitchenCompleted() {
             }
         } else {
             // Render loader if still loading.
-            // return <LoadingPopup/>;
-            return <HashLoader className={styles.hashLoader} color={"#3CC7D1"}/>;
+            // return <LoadingPopup/>;    // Overlay loader.
+            return <HashLoader className={styles.hashLoader} color={"#3CC7D1"}/>;    // "In-page" loader.
         }
     }
 
     return (
         <div className={styles.kitchenHome}>
-            <PageSwitcher pageNum={pageNum} totalPages={totalPages} setPageNum={setPageNum} />
+            <PageSwitcher pageNum={pageNum} totalPages={(data.numOrders / pageOrderLimit) + 1} setPageNum={setPageNum} />
             { renderCookedOrders() }
-            <PageSwitcher pageNum={pageNum} totalPages={totalPages} setPageNum={setPageNum} />
+            <PageSwitcher pageNum={pageNum} totalPages={(data.numOrders / pageOrderLimit) + 1} setPageNum={setPageNum} />
         </div>
     );
 };
