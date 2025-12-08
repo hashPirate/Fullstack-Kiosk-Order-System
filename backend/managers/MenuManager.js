d9fec8434e1229bcef07548e908091dc81f79e60
hashPirate <69019512+hashPirate@users.noreply.github.com>
2025-12-08 11:07:11 -0600

    Just realized this isn't needed

--- backend/managers/MenuManager.js
+++ backend/managers/MenuManager.js
@@ -189,9 +189,6 @@ ORDER BY mp.menu_part_id;`,
             if (quantity > 0) {
                 await this.db.runUpdate('INSERT INTO ingredients_to_menu_parts (ingredient_id, menu_part_id, quantity_cost) SELECT $1, $2, $3 WHERE NOT EXISTS (SELECT 1 FROM ingredients_to_menu_parts WHERE ingredient_id = $4 AND menu_part_id = $5)', [ingredient.getIngredientId(), menuPart.getMenuPartId(), quantity, ingredient.getIngredientId(), menuPart.getMenuPartId()]);
             }
-            else {
-                await this.db.runUpdate('DELETE FROM ingredients_to_menu_parts WHERE ingredient_id = $1 AND menu_part_id = $2', [ingredient.getIngredientId(), menuPart.getMenuPartId()]);
-            }
         }
     }
 }
