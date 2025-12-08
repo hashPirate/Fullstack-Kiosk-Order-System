164c3e375d53ec9d85cf00207ff508c4efd48104
Pjsansd <HarryHaihuapop@gmail.com>
2025-12-07 20:09:02 -0600

    Added order view documentation

--- frontend/src/views/cashier/CashierView/OrderView/OrderMenuPart.jsx
+++ frontend/src/views/cashier/CashierView/OrderView/OrderMenuPart.jsx
@@ -4,6 +4,23 @@ import { useContext } from "react";
 import styles from "./OrderView.module.css";
 import OrderContext from "../../OrderContext";
 
+/**
+ * Component that displays an individual menu part (modifier/add-on) belonging
+ * to a menu item in the order view.
+ *
+ * @component
+ * @param {Object} props
+ * @param {string} props.name - Name of the menu part.
+ * @param {number|string} props.price - Additional cost of this menu part.
+ * @param {number} props.itemIndex - Parent menu item index in `orderContent`.
+ * @param {number} props.partIndex - Index of this part inside the item's `parts` array.
+ *
+ * @description
+ * Clicking the remove icon removes this part from its parent item via context.
+ *
+ * @returns {JSX.Element} A styled row containing part name, price, and removal button.
+ */
+
 export default function OrderMenuPart({name, price, itemIndex, partIndex}) {
     const orderState = useContext(OrderContext);
 
